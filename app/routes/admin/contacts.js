585082653ec487589c7baebf687cfa6caac5a571
James Almeida <james.almeida.ti@gmail.com>
2018-10-26 21:48:30 -0300

    Homework 4
    adicionar funcao delete admin/contacts
    adicionar funcao delete admin/invitation

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -3,5 +3,15 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('contact');
+  },
+
+  actions: {
+    deleteContact(contact) {
+      let confirmation = confirm('Are you sure');
+
+      if (confirmation) {
+        contact.destroyRecord();
+      }
+    }
   }
 });
