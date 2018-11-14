54100e1db9b5786ce151d16f3d48f73b555f2e68
James Almeida <james.almeida.ti@gmail.com>
2018-11-14 09:54:05 -0200

    Adicionar metodo fetch torii na rota application para capturar estado da sessao

---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+
+export default Route.extend({
+  session: inject(),
+  
+  beforeModel: function () {
+    return this.get('session').fetch().catch(function () {});
+  }
+});
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,10 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
 
 export default Route.extend({
-  session: inject(),
   
-  beforeModel: function () {
-    return this.get('session').fetch().catch(function () {});
-  }
 });
